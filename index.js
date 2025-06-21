8aae39258f42b8098e87a59738025f76c03f7c09
anujda09 <anujdamani9@gmail.com>
2025-06-21 23:41:51 +0530

    a bit more

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const express = require("express")
 const app = express();
+app.use(express.json());
+const { UserModel, TodoModel } = require("Database/db.js");
+app.post('/signup', async function(req, res) {
 
-app.post('/signup', function(req, res) {
+    const name = req.body.name;
+    const password = req.body.password;
+    const email = req.body.email;
 
+    await UserModel.insert({
+        email: email,
+
+        password: password,
+        name: name
+    })
 });
 app.post('/signin', function(req, res) {
 
